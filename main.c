5d1701cccfcec8afa3addf597e74e295385971a0
VladZank <matthew.carroll@yandex.ru>
2017-02-19 11:58:24 +0700

    Implemented validating the entered data

--- main.c
+++ main.c
@@ -5,5 +5,20 @@ int main()
 	int date, amount;
 	printf("Input date and amount:");
 	scanf("%d %d", &date, &amount);
+	if(amount>=10000)
+	{
+		if(date>0)
+		{
+			
+		}
+		else
+		{
+			printf("Error: Incorrect date.\n");
+		}
+	}
+	else
+	{
+		printf("Error: Incorrect amount.\n");
+	}
 	return 0;
 }
