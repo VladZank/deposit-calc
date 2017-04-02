c5d39046a25fb89c679381e50e60fe87103095f8
VladZank <matthew.carroll@yandex.ru>
2017-04-02 12:51:49 +0700

    Test function Interest_income

--- test/depcalc_test.c
+++ test/depcalc_test.c
@@ -34,3 +34,61 @@ CTEST(Input_data, test7){
 CTEST(Input_data, test8){
 	ASSERT_EQUAL(-1, check(366, 10000));
 }
+
+//Interest income:
+
+CTEST(Income, test1){
+	ASSERT_EQUAL(-10000, interest_income(30, 100000));
+}
+
+CTEST(Income, test2){
+	ASSERT_EQUAL(-10000, interest_income(30, 100001));
+}
+
+CTEST(Income, test3){
+	ASSERT_EQUAL(2000, interest_income(31, 100000));
+}
+
+CTEST(Income, test4){
+	ASSERT_EQUAL(3000, interest_income(31, 100001));
+}
+
+CTEST(Income, test5){
+	ASSERT_EQUAL(2000, interest_income(120, 100000));
+}
+
+CTEST(Income, test6){
+	ASSERT_EQUAL(3000, interest_income(120, 100001));
+}
+
+CTEST(Income, test7){
+	ASSERT_EQUAL(6000, interest_income(121, 100000));
+}
+
+CTEST(Income, test8){
+	ASSERT_EQUAL(8000, interest_income(121, 100001));
+}
+
+CTEST(Income, test9){
+	ASSERT_EQUAL(6000, interest_income(240, 100000));
+}
+
+CTEST(Income, test10){
+	ASSERT_EQUAL(8000, interest_income(240, 100001));
+}
+
+CTEST(Income, test11){
+	ASSERT_EQUAL(12000, interest_income(241, 100000));
+}
+
+CTEST(Income, test12){
+	ASSERT_EQUAL(15000, interest_income(241, 100001));
+}
+
+CTEST(Income, test13){
+	ASSERT_EQUAL(12000, interest_income(365, 100000));
+}
+
+CTEST(Income, test14){
+	ASSERT_EQUAL(15000, interest_income(365, 100001));
+}
